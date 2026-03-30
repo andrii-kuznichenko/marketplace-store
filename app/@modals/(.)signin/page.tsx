52506c661bdf2258655e0826455e262f385e0610
andrii.kuznichenko <andriikuznichenko@gmail.com>
2026-03-30 18:33:41 +0200

    bug: vercel empty components error

--- app/@modals/(.)signin/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignInDialog } from '@/components/auth-form/SignInDialog';
-
-function SignIn({ children }: { children: React.ReactNode }) {
-  return (
-    <SignInDialog trigger={children}/>
-  );
-}
-
-export default SignIn;
--- app/@modals/default.tsx
+++
@@ -1,3 +0,0 @@
-export default function ModalsSlot() {
-  return null;
-}
