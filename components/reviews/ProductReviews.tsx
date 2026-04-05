17db9cce5cfc04918c540cfa76d32a445d4aa528
andrii.kuznichenko <andriikuznichenko@gmail.com>
2026-04-05 17:37:22 +0200

    fix: reviews errors

--- components/reviews/ProductReviews.tsx
+++ components/reviews/ProductReviews.tsx
@@ -11,12 +11,13 @@ async function ProductReviews({ productId }: { productId: string }) {
       <div className='grid md:grid-cols-2 gap-8 my-8'>
 
       {reviews.map((review) => {
-        const { comment, rating, authorImageUrl, authorName } = review;
+        const { comment, rating, authorImageUrl, authorName, productId } = review;
         const reviewInfo = {
           comment,
           rating,
           image: authorImageUrl,
           name: authorName,
+          productId,
         };
         return <ReviewCard key={review.id} reviewInfo={reviewInfo} />;
       })}
