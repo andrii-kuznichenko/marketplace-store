95c3e87a3ff5e9657b4be0165a86be5cac483fef
andrii.kuznichenko <andriikuznichenko@gmail.com>
2026-04-09 13:34:52 +0200

    fix: vercel error

--- app/(main)/checkout/page.tsx
+++ app/(main)/checkout/page.tsx
@@ -1,9 +1,12 @@
-import Checkout from '@/components/stripe/Checkout'
+import { Suspense } from 'react';
+import Checkout from '@/components/stripe/Checkout';
 
 export default function Page() {
   return (
-    <div id="checkout">
-      <Checkout />
+    <div id='checkout'>
+      <Suspense>
+        <Checkout />
+      </Suspense>
     </div>
-  )
+  );
 }
